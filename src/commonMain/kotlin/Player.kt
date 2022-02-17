10b5d431f89837fcd8cbea01721b52321b6b6af8
Tobse <Tobse4Git@gmail.com>
2022-02-17 22:53:45 +0100

    ✨ Add movable player

---
+++ src/commonMain/kotlin/Player.kt
@@ -0,0 +1,53 @@
+import com.soywiz.korev.Key
+import com.soywiz.korge.input.keys
+import com.soywiz.korge.view.BaseImage
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korui.layout.MathEx
+
+class Player(bitmap: Bitmap) : BaseImage(bitmap) {
+    private val step = 20
+
+    init {
+        initMoves()
+    }
+
+    private fun initMoves() {
+        keys {
+            keys {
+                down {
+                    if (it.key == Key.LEFT) {
+                        moveLeft()
+                    }
+                    if (it.key == Key.RIGHT) {
+                        moveRight()
+                    }
+                    if (it.key == Key.UP) {
+                        moveUp()
+                    }
+                    if (it.key == Key.DOWN) {
+                        moveDown()
+                    }
+                    if (it.key == Key.SPACE) {
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun moveLeft() {
+        x = MathEx.max(x - step, 0.0)
+    }
+
+    private fun moveRight() {
+        x = MathEx.min(x + step, virtualResolution.width.toDouble() - width)
+    }
+
+    private fun moveDown() {
+        y = MathEx.min(y + step, virtualResolution.height - height)
+    }
+
+    private fun moveUp() {
+        y = MathEx.max(y - step, 0.0)
+    }
+}
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -2,29 +2,54 @@ import com.soywiz.klock.seconds
 import com.soywiz.korge.Korge
 import com.soywiz.korge.tween.get
 import com.soywiz.korge.tween.tween
-import com.soywiz.korge.view.anchor
-import com.soywiz.korge.view.image
-import com.soywiz.korge.view.position
-import com.soywiz.korge.view.scale
+import com.soywiz.korge.view.*
+import com.soywiz.korgw.GameWindow
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.SizeInt
 import com.soywiz.korma.geom.degrees
 import com.soywiz.korma.interpolation.Easing
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+const val debug = false
+
+/**
+ * Virtual size which gets projected onto the [windowResolution]
+ */
+val virtualResolution = SizeInt(810, 1440)
+
+/**
+ * Actual window size
+ */
+val windowResolution = SizeInt(540, 960)
+
+val backgroundColor = Colors["#2b2b2b"]
+
+
+suspend fun main() = Korge(
+    virtualHeight = virtualResolution.height,
+    virtualWidth = virtualResolution.width,
+    width = windowResolution.width,
+    height = windowResolution.height,
+    bgcolor = backgroundColor,
+    debug = debug,
+    title = "KoJa Match Up",
+    quality = GameWindow.Quality.QUALITY,
+) {
     val minDegrees = (-16).degrees
     val maxDegrees = (+16).degrees
 
+    val playerImage = resourcesVfs["player-single.png"].readBitmap()
+    Player(playerImage).addTo(this)
+
     val image = image(resourcesVfs["korge.png"].readBitmap()) {
         rotation = maxDegrees
-        anchor(.5, .5)
-        scale(.8)
-        position(256, 256)
-    }
+        scale(.2)
+    }.alignRightToRightOf(this, 20).alignTopToTopOf(this, 20)
 
     while (true) {
         image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
         image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
     }
 }
+
